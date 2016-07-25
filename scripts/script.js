746133e222b665f1dd60cc303e439ce030fcf395
Nathan Long <git@natejlong.org>
2016-07-24 22:56:15 -0400

    fix sidebar styling so that there's a nice fixed header, shadows, and a fixed button

--- scripts/script.js
+++ scripts/script.js
@@ -4,29 +4,28 @@ window.onload = function() {
   var toggleHTML = '<div class="button" id="toc-toggle"> ' + '&#62;' + '</div>';
 
 
-  // document.getElementById("toc-sidebar").innerHTML =toggleHTML + document.getElementById("toc-inline").innerHTML;
   document.getElementById("toc-sidebar").innerHTML = document.getElementById("toc-inline").innerHTML;
 
 
   document.getElementById( 'toc-toggle' ).addEventListener( 'click', function() {
     if(showSidebar) {
       document.getElementById("sidebar").style.left = '-400px';
-      document.getElementById("sidebar").style.overflow = 'hidden';
+      document.getElementById("toc-sidebar").style.overflow = 'hidden';
       document.getElementById("toc-sidebar").style.opacity = '0';
       showSidebar = false;
-      // document.getElementById("toc-toggle").innerHTML = '&#62;'; 
     } else {
       document.getElementById("sidebar").style.left = '0px';
-      document.getElementById("sidebar").style.overflow = 'auto';
+      document.getElementById("toc-sidebar").style.overflow = 'auto';
       document.getElementById("toc-sidebar").style.opacity = '1';
       showSidebar = true;
-      // document.getElementById("toc-toggle").innerHTML = '&#60;';
     }
 
   }, false );
 
-  // document.getElementById( 'to-top-button' ).addEventListener( 'click', function() {
-    // window.scrollTo(0,1000);
-  // }, false );
+  document.getElementById('toc-sidebar').addEventListener('scroll', function() {
+    var opacity = document.getElementById('toc-sidebar').scrollTop;
+    opacity = (opacity > 20 ? 20 : opacity) / 100;
+    document.getElementById('sidebar-header').style.boxShadow = '0 5px 2px rgba(0, 0, 0, ' + opacity + ')';
+  }, false);
 }
 
